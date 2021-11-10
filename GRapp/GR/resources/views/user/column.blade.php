3f3e8ef6da952daba61516b95d9f8f6f59f5241c
DESKTOP-43UGCKU\sakamototaira <oic.sakamototaira@gmail.com>
2021-11-10 12:53:34 +0900

    マージ

---
+++ GRapp/GR/resources/views/user/column.blade.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>ケーキショップ向け 無料ホームページテンプレート tp_cute2</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="ここにサイト説明を入れます">
+    <meta name="keywords" content="キーワード１,キーワード２,キーワード３,キーワード４,キーワード５">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="js/openclose.js"></script>
+    <script src="js/fixmenu_pagetop.js"></script>
+    <!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+</head>
+
+<body>
+
+    <div id="container">
+
+        <header>
+            <h1 id="logo"><a href="index.html"><img src="images/logo2.png" alt="Macaroon Shop"></a></h1>
+            <h2>MACARRON<span>マカロンメニュー</span></h2>
+            <!--PC用（901px以上端末）メニュー-->
+            <nav id="menubar">
+                <ul>
+                    <li><a href="schedule"><span>schedule</span><span>スケジュール</span></a></li>
+                    <li><a href="budget"><span>budget</span><span>予算管理</span></a></li>
+                    <li><a href="column"><span>column</span><span>コラム特集</span></a></li>
+                    <li><a href="bridal_fair"><span>bridalfair</span><span>ブライダルフェア</span></a></li>
+                    <li><a href="chat"><span>chat</span><span>チャット</span></a></li>
+                    <li><a href="online_reserve"><span>consultation</span><span>オンライン相談予約</span></a></li>
+                </ul>
+                <ul class="icon">
+                    <li><a href="#"><img src="images/icon_facebook.png" alt="Facebook"></a></li>
+                    <li><a href="#"><img src="images/icon_twitter.png" alt="Twitter"></a></li>
+                    <li><a href="#"><img src="images/icon_instagram.png" alt="Instagram"></a></li>
+                    <li><a href="#"><img src="images/icon_youtube.png" alt="YouTube"></a></li>
+                    <li><a href="#"><img src="images/icon_line.png" alt="LINE"></a></li>
+                </ul>
+            </nav>
+        </header>
+
+        <!--小さな端末用（900px以下端末）ブロック-->
+        <nav id="menubar-s">
+            <ul>
+                <li><a href="schedule"><span>schedule</span><span>スケジュール</span></a></li>
+                <li><a href="budget"><span>budget</span><span>予算管理</span></a></li>
+                <li><a href="column"><span>column</span><span>コラム特集</span></a></li>
+                <li><a href="bridal_fair"><span>bridalfair</span><span>ブライダルフェア</span></a></li>
+                <li><a href="chat"><span>chat</span><span>チャット</span></a></li>
+                <li><a href="online_reserve"><span>consultation</span><span>オンライン相談予約</span></a></li>
+            </ul>
+            <ul class="icon">
+                <li><a href="#"><img src="images/icon_facebook.png" alt="Facebook"></a></li>
+                <li><a href="#"><img src="images/icon_twitter.png" alt="Twitter"></a></li>
+                <li><a href="#"><img src="images/icon_instagram.png" alt="Instagram"></a></li>
+                <li><a href="#"><img src="images/icon_youtube.png" alt="YouTube"></a></li>
+                <li><a href="#"><img src="images/icon_line.png" alt="LINE"></a></li>
+            </ul>
+        </nav>
+
+        <div id="main">
+
+            <section class="ofh">
+
+                <h2>マカロンメニュー</h2>
+
+                <div class="list">
+                    <figure><img src="images/sample1.jpg" alt=""></figure>
+                    <h4>マカロンタイトル</h4>
+                    <p class="price">150円</p>
+                    <p>ここに説明を入れます。サンプルテキスト。</p>
+                </div>
+
+                <div class="list">
+                    <figure><img src="images/sample2.jpg" alt=""></figure>
+                    <h4>マカロンタイトル</h4>
+                    <p class="price">150円</p>
+                    <p>ここに説明を入れます。サンプルテキスト。</p>
+                </div>
+
+                <div class="list">
+                    <figure><img src="images/sample1.jpg" alt=""></figure>
+                    <h4>マカロンタイトル</h4>
+                    <p class="price">150円</p>
+                    <p>ここに説明を入れます。サンプルテキスト。</p>
+                </div>
+
+                <div class="list">
+                    <figure><img src="images/sample2.jpg" alt=""></figure>
+                    <h4>マカロンタイトル</h4>
+                    <p>ここに説明を入れます。サンプルテキスト。</p>
+                </div>
+
+                <div class="list">
+                    <figure><img src="images/sample1.jpg" alt=""></figure>
+                    <h4>マカロンタイトル</h4>
+                    <p>ここに説明を入れます。サンプルテキスト。</p>
+                </div>
+
+                <div class="list">
+                    <figure><img src="images/sample2.jpg" alt=""></figure>
+                    <h4>マカロンタイトル</h4>
+                    <p>ここに説明を入れます。サンプルテキスト。</p>
+                </div>
+
+            </section>
+
+            <section>
+
+                <h2>※使い方注意</h2>
+
+                <h3>写真は正方形で準備して下さい</h3>
+                <p>長方形だと楕円形になってしまいます。<br>
+                    また、正方形だと思っていても1pxサイズが違うだけで段落ちする場合があるので必ず縦横サイズを統一して下さい。</p>
+
+                <h3>商品名タイトルや説明を長くしすぎないで下さい</h3>
+                <p>多く書き込むと閲覧する画面サイズによって切れます（↓）。</p>
+
+                <div class="list">
+                    <figure><img src="images/sample2.jpg" alt=""></figure>
+                    <h4>長くしすぎると途中で切れて見える場合があるので注意して下さい。</h4>
+                    <p>長くしすぎると途中で切れて見える場合があるので注意して下さい。</p>
+                </div>
+
+                <p class="clear">表示領域を増やしたい場合は、cssフォルダのsytle.cssの<br>
+                    #main .list h4 {<br>
+                    や、<br>
+                    #main .list p {<br>
+                    のheightを調整して下さい。説明も書いてあります。</p>
+                <p>尚、閲覧画面の幅が480px以下になると、３カラムから１カラムになるので、ここの切れてしまう制限は解除されています。</p>
+
+                <h3>写真の枠を円形ではなく、四角形や角丸四角形にもできます</h3>
+                <p>詳しくは、cssフォルダのstyle.cssの、<br>
+                    #main .list figure {<br>
+                    のブロックにあるborder-radiusの解説を見て下さい。
+                </p>
+
+            </section>
+
+        </div>
+        <!--/#main-->
+
+        <footer>
+            <small>Copyright&copy; <a href="index.html">Macaroon Shop</a> All Rights Reserved.</small>
+            <span class="pr"><a href="https://template-party.com/" target="_blank">《Web
+                    Design:Template-Party》</a></span>
+        </footer>
+
+    </div>
+    <!--/#container-->
+
+    <!--ページの上部に戻るボタン-->
+    <p class="nav-fix-pos-pagetop"><a href="#"><img src="images/icon_pagetop.png" alt="↑"></a></p>
+
+    <!--メニュー開閉ボタン-->
+    <div id="menubar_hdr" class="close"></div>
+
+    <!--メニューの開閉処理-->
+    <script>
+        open_close("menubar_hdr", "menubar-s");
+    </script>
+
+</body>
+
+</html>
