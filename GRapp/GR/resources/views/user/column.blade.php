6588135bbd16c77d81492992b4b9634efa503423
DESKTOP-1G80CP8\sakamototaira <oic.sakamototaira@gmail.com>
2021-11-16 14:00:44 +0900

    web。ｐｈｐ

--- GRapp/GR/resources/views/user/column.blade.php
+++ GRapp/GR/resources/views/user/column.blade.php
@@ -22,7 +22,9 @@
     <div id="container">
 
         <header>
-            <h1 id="logo"><a href="home"><img src="images/logo2.png" alt="Macaroon Shop"></a></h1>
+            <h1 id="logo">
+                <a href="home"><img src="images/logo2.png" alt="Macaroon Shop"></a>
+            </h1>
             <h2>COLUMN<span>コラム</span></h2>
             <!--PC用（901px以上端末）メニュー-->
             <nav id="menubar">
@@ -36,11 +38,21 @@
                     <li><a href="online_reserve"><span>consultation</span><span>オンライン相談予約</span></a></li>
                 </ul>
                 <ul class="icon">
-                    <li><a href="#"><img src="images/icon_facebook.png" alt="Facebook"></a></li>
-                    <li><a href="#"><img src="images/icon_twitter.png" alt="Twitter"></a></li>
-                    <li><a href="#"><img src="images/icon_instagram.png" alt="Instagram"></a></li>
-                    <li><a href="#"><img src="images/icon_youtube.png" alt="YouTube"></a></li>
-                    <li><a href="#"><img src="images/icon_line.png" alt="LINE"></a></li>
+                    <li>
+                        <a href="#"><img src="images/icon_facebook.png" alt="Facebook"></a>
+                    </li>
+                    <li>
+                        <a href="#"><img src="images/icon_twitter.png" alt="Twitter"></a>
+                    </li>
+                    <li>
+                        <a href="#"><img src="images/icon_instagram.png" alt="Instagram"></a>
+                    </li>
+                    <li>
+                        <a href="#"><img src="images/icon_youtube.png" alt="YouTube"></a>
+                    </li>
+                    <li>
+                        <a href="#"><img src="images/icon_line.png" alt="LINE"></a>
+                    </li>
                 </ul>
             </nav>
         </header>
@@ -57,11 +69,21 @@
                 <li><a href="online_reserve"><span>consultation</span><span>オンライン相談予約</span></a></li>
             </ul>
             <ul class="icon">
-                <li><a href="#"><img src="images/icon_facebook.png" alt="Facebook"></a></li>
-                <li><a href="#"><img src="images/icon_twitter.png" alt="Twitter"></a></li>
-                <li><a href="#"><img src="images/icon_instagram.png" alt="Instagram"></a></li>
-                <li><a href="#"><img src="images/icon_youtube.png" alt="YouTube"></a></li>
-                <li><a href="#"><img src="images/icon_line.png" alt="LINE"></a></li>
+                <li>
+                    <a href="#"><img src="images/icon_facebook.png" alt="Facebook"></a>
+                </li>
+                <li>
+                    <a href="#"><img src="images/icon_twitter.png" alt="Twitter"></a>
+                </li>
+                <li>
+                    <a href="#"><img src="images/icon_instagram.png" alt="Instagram"></a>
+                </li>
+                <li>
+                    <a href="#"><img src="images/icon_youtube.png" alt="YouTube"></a>
+                </li>
+                <li>
+                    <a href="#"><img src="images/icon_line.png" alt="LINE"></a>
+                </li>
             </ul>
         </nav>
 
@@ -72,11 +94,12 @@
                 <h2>カテゴリー別注目記事</h2>
 
                 <div class="list">
-                    <a href="column1">
+                    <a href="column/1">
                         <figure><img src="images/column/column.jpg" alt=""></figure>
                         <h4>オンライン婚活パーティって何？メリットや成功の秘訣</h4>
                         <p class="popular">人気</p>
-                    </a></br>
+                    </a>
+                    </br>
                     <div><a href="">婚活</a><a href="">マッチングアプリ　</a><a href="">すげー </a></div>
                     <p>ここに説明を入れます。サンプルテキスト。</p>
                 </div>
@@ -85,7 +108,8 @@
                         <figure><img src="images/sample1.jpg" alt=""></figure>
                         <h4>オンライン婚活パーティって何？メリットや成功の秘訣</h4>
                         <p class="popular">人気</p>
-                    </a></br>
+                    </a>
+                    </br>
                     <a href="">tag</a><a href="">tag</a><a href="">tag</a>
                     <p>ここに説明を入れます。サンプルテキスト。</p>
                 </div>
@@ -95,7 +119,8 @@
                         <figure><img src="images/sample1.jpg" alt=""></figure>
                         <h4>オンライン婚活パーティって何？メリットや成功の秘訣</h4>
                         <p class="popular">人気</p>
-                    </a></br>
+                    </a>
+                    </br>
                     <a href="">tag</a><a href="">tag</a><a href="">tag</a>
                     <p>ここに説明を入れます。サンプルテキスト。</p>
                 </div>
@@ -105,7 +130,8 @@
                         <figure><img src="images/sample1.jpg" alt=""></figure>
                         <h4>オンライン婚活パーティって何？メリットや成功の秘訣</h4>
                         <p class="popular">人気</p>
-                    </a></br>
+                    </a>
+                    </br>
                     <a href="">tag</a><a href="">tag</a><a href="">tag</a>
                     <p>ここに説明を入れます。サンプルテキスト。</p>
                 </div>
@@ -115,7 +141,8 @@
                         <figure><img src="images/sample1.jpg" alt=""></figure>
                         <h4>オンライン婚活パーティって何？メリットや成功の秘訣</h4>
                         <p class="popular">人気</p>
-                    </a></br>
+                    </a>
+                    </br>
                     <a href="">tag</a><a href="">tag</a><a href="">tag</a>
                     <p>ここに説明を入れます。サンプルテキスト。</p>
                 </div>
@@ -125,7 +152,8 @@
                         <figure><img src="images/sample1.jpg" alt=""></figure>
                         <h4>マカロンタイトル</h4>
                         <p class="popular">人気</p>
-                    </a></br>
+                    </a>
+                    </br>
                     <a href="">tag</a><a href="">tag</a><a href="">tag</a>
                     <p>ここに説明を入れます。サンプルテキスト。</p>
                 </div>
@@ -134,7 +162,8 @@
                         <figure><img src="images/sample1.jpg" alt=""></figure>
                         <h4>マカロンタイトル</h4>
                         <p class="popular">人気</p>
-                    </a></br>
+                    </a>
+                    </br>
                     <a href="">tag</a><a href="">tag</a><a href="">tag</a>
                     <p>ここに説明を入れます。サンプルテキスト。</p>
                 </div>
@@ -143,7 +172,8 @@
                         <figure><img src="images/sample1.jpg" alt=""></figure>
                         <h4>マカロンタイトル</h4>
                         <p class="popular">人気</p>
-                    </a></br>
+                    </a>
+                    </br>
                     <a href="">tag</a><a href="">tag</a><a href="">tag</a>
                     <p>ここに説明を入れます。サンプルテキスト。</p>
                 </div>
@@ -152,7 +182,8 @@
                         <figure><img src="images/sample1.jpg" alt=""></figure>
                         <h4>マカロンタイトル</h4>
                         <p class="popular">人気</p>
-                    </a></br>
+                    </a>
+                    </br>
                     <a href="">tag</a><a href="">tag</a><a href="">tag</a>
                     <p>ここに説明を入れます。サンプルテキスト。</p>
                 </div>
@@ -161,7 +192,8 @@
                         <figure><img src="images/sample1.jpg" alt=""></figure>
                         <h4>マカロンタイトル</h4>
                         <p class="popular">人気</p>
-                    </a></br>
+                    </a>
+                    </br>
                     <a href="">tag</a><a href="">tag</a><a href="">tag</a>
                     <p>ここに説明を入れます。サンプルテキスト。</p>
                 </div>
@@ -173,8 +205,8 @@
                 <h2>※使い方注意</h2>
 
                 <h3>写真は正方形で準備して下さい</h3>
-                <p>長方形だと楕円形になってしまいます。<br>
-                    また、正方形だと思っていても1pxサイズが違うだけで段落ちする場合があるので必ず縦横サイズを統一して下さい。</p>
+                <p>長方形だと楕円形になってしまいます。<br> また、正方形だと思っていても1pxサイズが違うだけで段落ちする場合があるので必ず縦横サイズを統一して下さい。
+                </p>
 
                 <h3>商品名タイトルや説明を長くしすぎないで下さい</h3>
                 <p>多く書き込むと閲覧する画面サイズによって切れます（↓）。</p>
@@ -185,17 +217,13 @@
                     <p>長くしすぎると途中で切れて見える場合があるので注意して下さい。</p>
                 </div>
 
-                <p class="clear">表示領域を増やしたい場合は、cssフォルダのsytle.cssの<br>
-                    #main .list h4 {<br>
-                    や、<br>
-                    #main .list p {<br>
-                    のheightを調整して下さい。説明も書いてあります。</p>
+                <p class="clear">表示領域を増やしたい場合は、cssフォルダのsytle.cssの<br> #main .list h4 {<br> や、
+                    <br> #main .list p {<br> のheightを調整して下さい。説明も書いてあります。
+                </p>
                 <p>尚、閲覧画面の幅が480px以下になると、３カラムから１カラムになるので、ここの切れてしまう制限は解除されています。</p>
 
                 <h3>写真の枠を円形ではなく、四角形や角丸四角形にもできます</h3>
-                <p>詳しくは、cssフォルダのstyle.cssの、<br>
-                    #main .list figure {<br>
-                    のブロックにあるborder-radiusの解説を見て下さい。
+                <p>詳しくは、cssフォルダのstyle.cssの、<br> #main .list figure {<br> のブロックにあるborder-radiusの解説を見て下さい。
                 </p>
 
             </section>
@@ -213,7 +241,9 @@
     <!--/#container-->
 
     <!--ページの上部に戻るボタン-->
-    <p class="nav-fix-pos-pagetop"><a href="#"><img src="images/icon_pagetop.png" alt="↑"></a></p>
+    <p class="nav-fix-pos-pagetop">
+        <a href="#"><img src="images/icon_pagetop.png" alt="↑"></a>
+    </p>
 
     <!--メニュー開閉ボタン-->
     <div id="menubar_hdr" class="close"></div>
